392c5a93b1498f1d67e8b21bf57430302c33a598
EliJ91 <59631669+EliJ91@users.noreply.github.com>
2021-02-18 14:02:46 -0500

    Fixed API calls from localhost to webhost env variable

--- src/Components/caravanReportsLogic.js
+++ src/Components/caravanReportsLogic.js
@@ -6,62 +6,62 @@ const useCaravanReports = () => {
 
     const [playerSearch, setPlayerSearch] = useState()
     const [guildSearch, setGuildSearch] = useState()
-    
-    let playerArray = playerSearch ? reports.filter((report) => report.username.toUpperCase().includes(playerSearch.toUpperCase())) : reports 
+
+    let playerArray = playerSearch ? reports.filter((report) => report.username.toUpperCase().includes(playerSearch.toUpperCase())) : reports
     let guildArray = guildSearch ? reports.filter((report) => report.guild.toUpperCase().includes(guildSearch.toUpperCase())) : reports
     let array = playerSearch ? playerArray : guildArray
 
     useEffect(()=>{
         async function fetchData(){
-            await axios.get("http://localhost:5000/api/caravan_report/caravanreports") 
+            await axios.get(process.env.REACT_APP_API_PREFIX+"/api/caravan_report/caravanreports")
             .then(function (response) {
-                setReports(response.data[0])   
+                setReports(response.data[0])
               })
               .catch(function (error) {
                 console.log(error);
-              })             
-            
+              })
+
         }
-        fetchData()                       
+        fetchData()
     },[])
 
     async function updatePaid(value,id){
-      await axios.get("http://localhost:5000/api/caravan_report/updatepaid",{ params: { value, id }}) 
+      await axios.get(process.env.REACT_APP_API_PREFIX+"/api/caravan_report/updatepaid",{ params: { value, id }})
       .then(function (response) {
-          setReports(response.data[0])   
+          setReports(response.data[0])
           console.log(response)
         })
         .catch(function (error) {
           console.log(error);
-        })          
+        })
     }
 
     async function updateFine(value,id){
-      await axios.get("http://localhost:5000/api/caravan_report/updatefine",{ params: { value, id }}) 
+      await axios.get(process.env.REACT_APP_API_PREFIX+"/caravan_report/updatefine",{ params: { value, id }})
       .then(function (response) {
-          setReports(response.data[0])   
+          setReports(response.data[0])
           console.log(response)
         })
         .catch(function (error) {
           console.log(error);
-        })          
+        })
     }
 
     async function updateNotes(value,id){
-      await axios.get("http://localhost:5000/api/caravan_report/updatenote",{ params: { value, id }}) 
+      await axios.get(process.env.REACT_APP_API_PREFIX+"/api/caravan_report/updatenote",{ params: { value, id }})
       .then(function (response) {
-          setReports(response.data[0])   
+          setReports(response.data[0])
           console.log(response)
         })
         .catch(function (error) {
           console.log(error);
-        })          
+        })
     }
 
-   
 
 
-    
+
+
     return {array,setPlayerSearch,setGuildSearch,playerSearch,updatePaid,updateFine,updateNotes}
 }
 
